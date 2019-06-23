5b6bd8a495b34580fed312d4d69e221354746df6
Anna Abad Sieper <anna.alicia@freenet.de>
2019-06-23 18:32:54 +0200

    log rtu modbus

--- webserver/app.py
+++ webserver/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
 from flask_restful import Api, Resource, reqparse
-from pymodbus.client.sync import ModbusTcpClient, ModbusClient
+from pymodbus.client.sync import ModbusTcpClient, ModbusSerialClient
+
+import logging
+FORMAT = ('%(asctime)-15s %(threadName)-15s '
+          '%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'geheim'
@@ -9,6 +16,8 @@ toggled1 = 'Toggled1'
 untoggled1 = 'Untoggled1'
 toggled2 = 'Toggled2'
 untoggled2 = 'Untoggled2'
+
+UNIT = 0x01
 
 @app.route('/')
 def index():
@@ -22,20 +31,27 @@ def toggled_rtu_status():
     state = args["status"]
 
     if state == untoggled1:
-        client = ModbusClient(method='rtu', port='/dev/ttyS0', timeout=1)
-        client.write_coil(1, True)
-        result = client.read_coils(1,1)
-        print result.bits[0]
+        client = ModbusSerialClient(method='rtu', port='/dev/ttyS0', timeout=1, baudrate=9600)
+	client.connect()
+	log.debug("Writing Coils")
+        rq = client.write_coil(1, True, unit=UNIT)
+	log.debug(rq)
+	log.debug("Reading Coils")
+        rr = client.read_coils(1,1, unit=UNIT)
+	log.debug(rr)
         client.close()
 
         new_state = toggled1
         print new_state
         return new_state
     else:
-        client = ModbusClient(method='rtu', port='/dev/ttyS0', timeout=1)
-        client.write_coil(1, False)
-        result = client.read_coils(1,1)
-        print result.bits[0]
+        client = ModbusSerialClient(method='rtu', port='/dev/ttyS0', timeout=1, baudrate=9600)
+	log.debug("Writing Coils")
+        rq = client.write_coil(1, False, unit=UNIT)
+	log.debug(rq)
+	log.debug("Reading Coils")
+        rr = client.read_coils(1,1, unit=UNIT)
+	log.debug(rr)
         client.close()
 
         new_state = untoggled1
